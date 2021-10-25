eff1f00256face8dd611d975e5f17a222af868ee
CYRIL VELLA <cyril.vella@yahoo.com>
2021-10-25 15:42:35 +0900

    first commit

---
+++ lib/shared/navbar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NavbarGlobal extends StatefulWidget {
+  const NavbarGlobal({Key? key}) : super(key: key);
+
+  @override
+  _NavbarGlobalState createState() => _NavbarGlobalState();
+}
+
+class _NavbarGlobalState extends State<NavbarGlobal> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('ok'));
+  }
+}
